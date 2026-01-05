44d6bb9041ceff4e7fcee673e5b22d23f1cf6db9
Hassan Mahdy <hassanmahdy1433@gmail.com>
2026-01-05 18:50:58 +0200

    edite core middleware

--- middlewares/protect.js
+++ middlewares/protect.js
@@ -1,22 +1,28 @@
 import jwt from "jsonwebtoken";
 import User from "../models/userModel.js";
 import AppError from "../utils/appError.js";
+import { promisify } from "util";
 
-
-
-
-export const verfyJWT = (req, res, next) => {
+export const verfyJWT = async(req, res, next) => {
+  let token;
   const authHeader = req.headers.authorization || req.headers.Authorization;
   if (!authHeader?.startsWith("Bearer ")) {
     return next(new AppError("UnAuthorized", 401));
   }
-  const token = authHeader.split(" ")[1];
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, async (err, decoded) => {
-    if (err) return next(new AppError("forbidden token", 403));
-    const currentUser = await User.findById(decoded.currentUser.id);
-    if (!currentUser) {
-      return next(new AppError("User no longer exists", 401));
-    }
+  token = authHeader.split(" ")[1];
+  if (!token) {
+    return next(
+      new AppError("You are not logged in! Please log in to get access.", 401)
+    );
+  }
+// باستخدام promisify عشان نحولها لـ Promise
+  const decoded = await promisify(jwt.verify)(token, process.env.ACCESS_TOKEN_SECRET);
+
+  const currentUser = await User.findById(decoded.currentUser.id);
+  
+  if (!currentUser) {
+    return next( new AppError("User no longer exists", 401));
+  }
     if (currentUser.changedPasswordAfter(decoded.iat)) {
       return next(
         new AppError(
@@ -25,13 +31,10 @@ export const verfyJWT = (req, res, next) => {
         )
       );
     }
-    
+
     req.user = currentUser;
     next();
-  });
-};
-
-
+  }
 
 export const restrictTo = (...roles) => {
   return (req, res, next) => {
